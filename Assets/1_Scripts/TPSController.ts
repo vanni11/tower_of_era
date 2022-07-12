189ebe9db2fdf14a62b0d5915d3c4fbafc85f862
yuntaehun <varick137@gmail.com>
2022-07-12 19:48:41 +0900

    게임진행 프로토타입 v1.0

--- Assets/1_Scripts/TPSController.ts
+++ Assets/1_Scripts/TPSController.ts
@@ -1,38 +1,46 @@
-import { Quaternion, Transform, Vector3 } from 'UnityEngine';
+import { GameObject, Light, Quaternion, Transform, Vector3 } from 'UnityEngine';
 import { ZepetoCamera, ZepetoCharacter, ZepetoPlayers, ZepetoScreenTouchpad } from 'ZEPETO.Character.Controller';
 import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
 
 export default class TPSController extends ZepetoScriptBehaviour {
 
     public myCharacter: ZepetoCharacter;
-    public myCamera: ZepetoCamera;
-
-    public tower: Transform;
 
     @Header("Camera Angle")
-    public x:int;
-    public y:int;
-    public z:int;
+    public myCamera: ZepetoCamera;
+    public cameraX: int;
+    public cameraY: int;
+    public cameraZ: int;
+
+    public tower: GameObject;
+
+    @Header("Light Position")
+    public light: Light;
+    public lightX: int;
+    public lightY: int;
+    public lightZ: int;
 
     Start() {
         ZepetoPlayers.instance.OnAddedLocalPlayer.AddListener(() => {
             this.myCharacter = ZepetoPlayers.instance.LocalPlayer.zepetoPlayer.character;
             this.myCamera = ZepetoPlayers.instance.LocalPlayer.zepetoCamera;
         });
-
-
     }
 
     Update() {
-        if(null == this.myCharacter || null == this.myCamera) {
-            return;
-        }
-
-        var targetposition = new Vector3(this.tower.transform.position.x, this.myCharacter.transform.position.y, this.tower.transform.position.z);
-        this.myCamera.cameraParent.transform.LookAt(targetposition);
-        this.myCamera.cameraParent.transform.Rotate(new Vector3(this.x,this.y,this.z));
-    }
-
+        try {
+            if(null == this.myCharacter || null == this.myCamera) {
+                return;
+            }
     
+            var targetposition = new Vector3(this.tower.transform.position.x, this.myCharacter.transform.position.y, this.tower.transform.position.z);
+            this.myCamera.cameraParent.transform.LookAt(targetposition);
+            this.myCamera.cameraParent.transform.Rotate(new Vector3(this.cameraX, this.cameraY, this.cameraZ));
+
+            this.light.transform.position = new Vector3(this.myCharacter.transform.position.x + this.lightX, this.myCharacter.transform.position.y + this.lightY, this.myCharacter.transform.position.z + this.lightZ);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
 }
